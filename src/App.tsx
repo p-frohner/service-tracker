f70c35371a0ecb28a6e5bd75f7601ef50df56b44
Peter Frohner <>
2026-01-05 15:33:03 +0100

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
+import { Box, Container } from "@mui/material";
+import logo from "/logo.png";
 
-import reactLogo from "./assets/react.svg";
-
-import "./App.css";
-
-function App() {
-	const [count, setCount] = useState(0);
-
+export const App = () => {
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noopener">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noopener">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)} type="button">
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+		<Container maxWidth="lg">
+			<Box
+				component="img"
+				src={logo}
+				sx={{ display: { xs: "block", md: "none", objectFit: "none", objectPosition: "right" } }}
+				width={110}
+				height={72}
+			/>
+			<Box
+				component="img"
+				src={logo}
+				sx={{ display: { xs: "none", md: "block", objectFit: "fill", objectPosition: "center" } }}
+				width={558}
+				height={72}
+			/>
+		</Container>
 	);
-}
-
-export default App;
+};
