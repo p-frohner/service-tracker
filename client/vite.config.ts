20e18d1961d7dec83a7822c01aadd726e60cacdf
Peter Frohner <>
2026-01-06 10:08:11 +0100

    fix cors for dev

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,14 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // fix CORS temporarily, need a proper backend setup for production
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
