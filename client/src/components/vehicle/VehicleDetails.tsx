71a8d01b9f6bfee143969d5dd8a24e18e9c62ddd
Peter Frohner <160716120+p-frohner@users.noreply.github.com>
2026-01-27 14:49:13 +0100

    move delete action behind a menu

--- client/src/components/vehicle/VehicleDetails.tsx
+++ client/src/components/vehicle/VehicleDetails.tsx
@@ -1,3 +1,5 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import SettingsIcon from "@mui/icons-material/Settings";
 import {
 	Box,
 	Button,
@@ -5,10 +7,16 @@ import {
 	DialogActions,
 	DialogContent,
 	DialogTitle,
+	IconButton,
+	ListItemIcon,
+	ListItemText,
+	Menu,
+	MenuItem,
 	Stack,
 } from "@mui/material";
 import { useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "@tanstack/react-router";
+import { useState } from "react";
 import { getListVehiclesQueryKey, useDeleteVehicle, useGetVehicle } from "../../api";
 import { useVehicleImages } from "../../hooks/useVehicleImages";
 import { Route } from "../../routes/vehicle-details.$vehicleId";
@@ -34,13 +42,41 @@ export const VehicleDetails = () => {
 			},
 		},
 	});
+	const [menuAnchorEl, setMenuAnchorEl] = useState<HTMLElement | null>(null);
+	const menuOpen = Boolean(menuAnchorEl);
 
 	const vehicleName = vehicle ? `${vehicle.make} ${vehicle.model} - ${vehicle.year}` : "";
 
 	return (
 		<Container maxWidth="md">
 			<Stack gap={2} p={2}>
-				<Breadcrumbs items={[{ label: "Vehicles", url: "/" }, { label: vehicleName }]} />
+				<Box display="flex" alignItems="center" justifyContent="space-between">
+					<Breadcrumbs items={[{ label: "Vehicles", url: "/" }, { label: vehicleName }]} />
+					<IconButton onClick={(e) => setMenuAnchorEl(e.currentTarget)}>
+						<SettingsIcon />
+					</IconButton>
+					<Menu
+						anchorEl={menuAnchorEl}
+						open={menuOpen}
+						onClose={() => setMenuAnchorEl(null)}
+						slotProps={{ paper: { elevation: 3 } }}
+						sx={{
+							"& .MuiBackdrop-root": { backgroundColor: "transparent", backdropFilter: "none" },
+						}}
+					>
+						<MenuItem
+							onClick={(e) => {
+								setMenuAnchorEl(null);
+								deleteDialog.open(e);
+							}}
+						>
+							<ListItemIcon>
+								<DeleteIcon color="error" />
+							</ListItemIcon>
+							<ListItemText>Delete</ListItemText>
+						</MenuItem>
+					</Menu>
+				</Box>
 				<Carousel
 					images={images}
 					onDownloadImages={downloadImages}
@@ -60,54 +96,42 @@ export const VehicleDetails = () => {
 				</Box>
 				<MaintenanceRecordList vehicleId={vehicleId} />
 			</Stack>
-			<Box sx={{ py: 3 }}>
-				<Button
-					type="submit"
-					variant="contained"
-					color="error"
-					fullWidth
-					size="large"
-					onClick={deleteDialog.open}
-				>
-					Delete Vehicle
-				</Button>
-				<Dialog
-					open={deleteDialog.isOpen}
-					anchorEl={deleteDialog.anchorEl}
-					onClose={deleteDialog.close}
-				>
-					<DialogTitle>Are you sure?</DialogTitle>
-					<DialogActions sx={{ px: 3, pb: 3 }}>
-						<Button onClick={() => deleteDialog.close()} variant="outlined" fullWidth>
-							Close
-						</Button>
-						<Button
-							variant="contained"
-							onClick={() => {
-								deleteVehicle({ vehicleId });
-								deleteDialog.close();
-							}}
-							fullWidth
-						>
-							Yes
-						</Button>
-					</DialogActions>
-				</Dialog>
-				<Dialog
-					open={addMaintenanceRecordDialog.isOpen}
-					anchorEl={addMaintenanceRecordDialog.anchorEl}
-					onClose={addMaintenanceRecordDialog.close}
-				>
-					<DialogTitle>New Maintenance</DialogTitle>
-					<DialogContent>
-						<AddMaintenanceRecord
-							onSubmit={() => {
-								addMaintenanceRecordDialog.close();
-							}}
-						/>
-					</DialogContent>
-				</Dialog>
-			</Box>
+			<Dialog
+				open={deleteDialog.isOpen}
+				anchorEl={deleteDialog.anchorEl}
+				onClose={deleteDialog.close}
+			>
+				<DialogTitle>Are you sure?</DialogTitle>
+				<DialogActions sx={{ px: 3, pb: 3 }}>
+					<Button onClick={() => deleteDialog.close()} variant="outlined" fullWidth>
+						Close
+					</Button>
+					<Button
+						variant="contained"
+						onClick={() => {
+							deleteVehicle({ vehicleId });
+							deleteDialog.close();
+						}}
+						fullWidth
+					>
+						Yes
+					</Button>
+				</DialogActions>
+			</Dialog>
+			<Dialog
+				open={addMaintenanceRecordDialog.isOpen}
+				anchorEl={addMaintenanceRecordDialog.anchorEl}
+				onClose={addMaintenanceRecordDialog.close}
+			>
+				<DialogTitle>New Maintenance</DialogTitle>
+				<DialogContent>
+					<AddMaintenanceRecord
+						onSubmit={() => {
+							addMaintenanceRecordDialog.close();
+						}}
+					/>
+				</DialogContent>
+			</Dialog>
 		</Container>
 	);
 };
